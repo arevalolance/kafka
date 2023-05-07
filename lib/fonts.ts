5146e217334c5d67d5bb318f1efd7ce5647be67b
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-05-08 00:40:32 +0800

    refactor: font vercel fix

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,13 @@
 import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
 
 export const fontSans = FontSans({
+  display: "swap",
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
 export const fontMono = FontMono({
+  display: "swap",
   subsets: ["latin"],
   variable: "--font-mono",
 })
